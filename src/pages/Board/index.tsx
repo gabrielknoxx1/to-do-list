bf64b29d4bfb5aeaabe2b5dd577685324bc35ced
Gabriel Sousa <byelg.sousa@gmai.com>
2023-07-20 00:19:09 -0300

    FEAT: Page Board

---
+++ src/pages/Board/index.tsx
@@ -0,0 +1,25 @@
+import { TaskForm } from "../../components/TaskForm";
+import { CardTask } from "../../components/CardTask";
+import { useTaskListStore } from "../../services/stores/useTaskListStore";
+import { Container, Content } from "./styles";
+import { useEffect } from "react";
+import { useApi } from "../../services/api";
+export const Board = () => {
+  const [tasklistState] = useTaskListStore(({ tasklist }) => [tasklist]);
+  const { getTasks } = useApi();
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  return (
+    <Container>
+      <TaskForm />
+      <Content>
+        {tasklistState.map((task) => (
+          <CardTask key={task.id} {...task} />
+        ))}
+      </Content>
+    </Container>
+  );
+};
---
+++ src/pages/Board/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+export const Content = styled.div`
+  flex: 1;
+  border: 1px solid ${({ theme }) => theme["gray-500"]};
+  margin: 8rem;
+  border-radius: 0.5rem;
+  padding: 1rem;
+  overflow-x: hidden;
+`;
