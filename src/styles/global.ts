b6ef5cdd9070f4549bc36e18723758f1fee7c4de
Gabriel Sousa <byelg.sousa@gmai.com>
2023-07-20 00:15:49 -0300

    FEAT: add global style and theme

---
+++ src/styles/global.ts
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px){
+      font-size: 93.75%;
+    }
+    @media (max-width: 720px){
+      font-size: 83.5%;
+    }
+  }
+
+  body {
+
+    background-color: ${(props) => props.theme["gray-600"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+
+::-webkit-scrollbar {
+  width: 4px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey;
+  border-radius: 10px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: ${({ theme }) => theme["purple-500"]};
+  border-radius: 10px;
+}
+`;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,26 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-100': '#F2F2F2',
+  'gray-200': '#D9D9D9',
+  'gray-300': '#808080',
+  'gray-400': '#333333',
+  'gray-500': '#262626',
+  'gray-600': '#1A1A1A',
+  'gray-700': '#0D0D0D',
+
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'red-500': '#E25858',
+
+  'blue-300': '#4EA8DE',
+  'blue-400': '#1E6F9F',
+
+  'purple-400': '#8284FA',
+  'purple-500': '#5E60CE',
+}
+
+
