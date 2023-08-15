c037b59cd407b1fef36fde450d418ef2f00528b5
Gabriel Knoxx <byelg.sousa@gmail.com>
2023-08-15 16:02:08 -0300

    chore:  basic integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "styled-components";
 import { Board } from "./pages/Board";
-import { defaultTheme } from "./styles/themes/default";
+import { client } from "./services/api/apollo";
 import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Board />
-      <GlobalStyle />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={defaultTheme}>
+        <Board />
+        <GlobalStyle />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,25 +1,48 @@
-import { TaskForm } from "../../components/TaskForm";
-import { CardTask } from "../../components/CardTask";
-import { useTaskListStore } from "../../services/stores/useTaskListStore";
-import { Container, Content } from "./styles";
 import { useEffect } from "react";
+import { CardTask } from "../../components/CardTask";
+import { TaskForm } from "../../components/TaskForm";
 import { useApi } from "../../services/api";
+import { useGetTodosQuery } from "../../services/api/@generated/schemas";
+import {
+  TaskListProps,
+  useTaskListStore,
+} from "../../services/stores/useTaskListStore";
+import { Container, Content } from "./styles";
+
 export const Board = () => {
-  const [tasklistState] = useTaskListStore(({ tasklist }) => [tasklist]);
+  const [tasklistState, updateList] = useTaskListStore(
+    ({ tasklist, updateList }) => [tasklist, updateList]
+  );
   const { getTasks } = useApi();
+  const { data, error, loading } = useGetTodosQuery();
+
+  const moreTask = data?.todos?.map((content) => {
+    return {
+      id: content.id,
+      title: content.title,
+      description: content.description,
+      status: content.completed ? "done" : "pending",
+    };
+  }) as TaskListProps[];
 
   useEffect(() => {
     getTasks();
   }, []);
 
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
   return (
     <Container>
       <TaskForm />
       <Content>
-        {tasklistState.map((task) => (
+        {tasklistState?.map((task) => (
           <CardTask key={task.id} {...task} />
         ))}
-        {tasklistState?.length === 0 && (
+        <p>Apenas visualização</p>
+        {moreTask?.map((task) => (
+          <CardTask key={task.id} {...task} />
+        ))}
+        {tasklistState?.length === 0 && moreTask?.length === 0 && (
           <strong>Sem Tarefas cadastradas</strong>
         )}
       </Content>
